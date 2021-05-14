dbb7d35e7795eb5238537f664f0577742f01a92c
Armaan Abraham <armaanabraham@g.ucla.edu>
2021-05-14 15:52:09 -0700

    Initialized amplify backend with basic auth
    Initialized the backend with amplify through the amplify CLI. Added the ability to
    create users with a username, email, phone number, and password by adding auth through
    amplify. Deployed the amplify directory (using amplify push). Installed react
    dependencies aws-amplify and aws-amplify/ui-react. Using the auth ui components
    provided by the latter package, a basic frontend interface for user management
    was added to the react app, allowing for signup, login, and logout. Information and
    references regarding amplify added to the README file in the amplify folder.

--- cs35l-project-react-app/src/App.js
+++ cs35l-project-react-app/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <AmplifySignOut />
+            {/* optional sign out button */}
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
+/* provides login functionality for the entire app 
+ * see https://docs.amplify.aws/lib/auth/getting-started/q/platform/js#enable-sign-up-sign-in-and-sign-out 
+ * The Amplify docs say that we can customize login and signup frontend 
+ * functionality. I initially wanted a way to login by just providing email and password, without any email
+ * verification. But after a few hours of trying that I gave up. Amplify made it hard and I don't think it's 
+ * that bad of a thing to require email verification. 
+ *
+ * One thing we can definitely do to customize the login and signup screen is to just do some custom css to 
+ * modify the imported ui components as described here: https://docs.amplify.aws/ui/customization/theming/q/framework/react.
+ * 
+ * */
