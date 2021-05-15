bf0a2aa8fec2e095869d21a48fbbbcd6af6cd03f
Rowan McKereghan <rowanmckereghan@gmail.com>
2021-05-15 16:45:46 -0700

    Image Upload and Handler skeleton
    Created ImgApp class for uploading and posting images.
    Unsure what backend to use for handling cloud links and image storage, so just put axios for now. Probably best to replace it for AWS when the backend people figure it out.

--- cs35l-project-react-app/src/index.js
+++ cs35l-project-react-app/src/index.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './App.css';
+import logo from './logo.svg'; //important for handling images
+import axios from 'axios'; //important for handling images (replace with AWS image handler?)
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
+class ImgApp extends Component {
+  state = {
+    selectedFile: null
+  }
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('image', this.state.selectedFile, this.state.selectedFile.name)
+    axios.post("", fd, {onUploadProgress: progressEvent => { //this is what we would replace with AWS if we can; here is where we put a link for storing data in backend
+      console.log("Upload Progress: " + Math.round(progressEvent.loaded / progressEvent.total * 100))}}) 
+      .then(res => {console.log(res);}); 
+  }
+
+  fileSelectedHandler = event => {
+    this.setState({
+    selectedFile: event.target.files[0]})
+  }
+
+  render()
+  {
+    return (
+      <div className="ImgApp">
+        <input type="file" onChange={this.fileSelectedHandler}/>
+        <button onClick={this.fileUploadHandler}>Upload</button>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ImgApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
