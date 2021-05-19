e93c55c0e001644ae28747a927a07db61495f347
Armaan Abraham <armaanabraham@g.ucla.edu>
2021-05-19 10:26:58 -0700

    replaced amplify with firebase
    Merged new version into origin/armaan using the 'theirs' option,
    overriding changes made with amplify with changes made with
    firebase.

--- cs35l-project-react-app/src/App.js
+++ cs35l-project-react-app/src/App.js
@@ -9,15 +9,4 @@ const App = () => {
 };
 
 
-export default withAuthenticator(App);
-/* provides login functionality for the entire app 
- * see https://docs.amplify.aws/lib/auth/getting-started/q/platform/js#enable-sign-up-sign-in-and-sign-out 
- * The Amplify docs say that we can customize login and signup frontend 
- * functionality. I initially wanted a way to login by just providing email and password, without any email
- * verification. But after a few hours of trying that I gave up. Amplify made it hard and I don't think it's 
- * that bad of a thing to require email verification. 
- *
- * One thing we can definitely do to customize the login and signup screen is to just do some custom css to 
- * modify the imported ui components as described here: https://docs.amplify.aws/ui/customization/theming/q/framework/react.
- * 
- * */
+export default App;
