3ffd83d6c804c55b28e65fd1cfa13ed0c084d57f
Dane Payba <dpayba01@gmail.com>
2021-05-26 10:03:01 -1000

    Start of routing branch

--- cs35l-project-react-app/src/index.js
+++ cs35l-project-react-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { storage } from './firebase';
+import {Router, Route, browserHistory, IndexRoute} from "react-router"; 
+import {Auth} from "./components/auth";
+import {Upload} from "./components/file-upload";
+import {Posts} from "./components/posts";
 
 ReactDOM.render(
   <React.StrictMode>
