6a76e3afe42e2a8e46d1a52c05d58ba51b8a3f18
Dane Payba <dpayba01@gmail.com>
2021-05-26 16:57:45 -1000

    Firebase upload link created

--- cs35l-project-react-app/src/App.js
+++ cs35l-project-react-app/src/App.js
@@ -21,6 +21,7 @@ const App = () => {
                     <Route path="/login" component={SignIn} />
                     <Route path="/signup" component={SignUp} />
                     <Route path="/upload" component={FirebaseUpload} />
+                    <Route path="/feed" render={<Post postId="-Ma_8hv3-wFTqCjCHw3U" currentUserId="aTSKynFlfWP1AGWkjdGsndlgIXi2" />}/>
                 </Switch>
             </div>
         </Router>
