9dc2bb7c8a320af893a65fe0032c430871222b1c
Kelly Chan <kelmchan24@gmail.com>
2021-06-01 12:48:25 -0700

    constructor

---
+++ cs35l-project-react-app/src/components/chat.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { storage } from '../firebase';
+import db from '../firebase';
+import App from '../App';
+
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: auth().currentUser,
+            chats: [],
+            readError: null,
+            writeError: null
+        };
+
+        this.handleChange = this.hangleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        this.setState({ readError: null });
+        try {
+            db.ref("chats").on("value", snapshot => {
+                let chats = [];
+                snapshot.forEach((snap) => {
+                    chats.push(snap.val());
+                })
+                this.setState({ chats });
+            });
+        } catch (error) {
+            this.setState({ readError: error.message });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="chats">
+                    {this.state.chats.map(chat => {
+                        return <p key={chat.timestamp}>{chat.content}</p>
+                    })}
+                </div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleChange} value={this.state.content}></input>
+                    {this.state.error ? <p>{this.state.writeError}</p> : null}
+                    <button type="submit">Send</button>
+                </form>
+
+                <div>
+                    Login in as: <strong>{this.state.user.email}</strong>
+                </div>
+            </div>
+        );
+    }
+
+    handleChange(event) {
+        this.setState({
+            content: event.target.value
+        });
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        this.setState({ writeError: null });
+        try {
+            await db.ref("chats").push({
+                content: this.state.content,
+                timestamp: Date.now(),
+                uid: this.state.user.uid
+            });
+            this.setState({ content: '' });
+        } catch (error) {
+            this.setState({ writeError: error.message });
+        }
+    }
+}
+
